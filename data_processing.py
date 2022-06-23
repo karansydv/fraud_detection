736e6607086ac79b926ad3ec2f2d546922bec367
karansydv <karan.s.1998@gmail.com>
2022-06-23 23:41:27 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,9 @@
 print("adding all the data processing code related to fraud_detection")
+
+a = 1
+b = 2
+
+def sum(a,b):
+    return a+b
+
+print("added a new function.........which has to be removed")
